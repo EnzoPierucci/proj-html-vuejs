7bf47139d5a8482c98152b2de9a9ef232c63b015
EnzoPierucci <enzopierucci@gmail.com>
2024-02-20 15:50:32 +0100

    Header fix

--- proj-html-vuejs/src/components/Header.vue
+++ proj-html-vuejs/src/components/Header.vue
@@ -1,32 +1,35 @@
 <template>
-    <header class="header">
-      <nav class="navbar">
+    <header class="bg-white text-gray-800 shadow-md">
+      <nav class="container mx-auto flex items-center justify-between py-4">
         <!-- Logo -->
-        <a href="/" class="navbar-brand">
-          <img src="../assets/logo-autocar11.png" alt="Logo" />
+        <a href="/" class="flex items-center">
+          <img src="../assets/logo-autocar11.png" alt="Logo" class="h-12 mr-2" />
         </a>
         
         <!-- Links di navigazione -->
-        <ul class="navbar-nav">
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About Us</a></li>
-          <li><a href="#vehicles">Vehicles</a></li>
-          <li><a href="#dealers">Dealers</a></li>
-          <li><a href="#packages">Packages</a></li>
-          <li><a href="#blog">Blog</a></li>
-          <li><a href="#contact">Contact</a></li>
-          
+        <ul class="flex space-x-4">
+          <li><a href="#home" class="hover:text-gray-500">Home</a></li>
+          <li><a href="#about" class="hover:text-gray-500">About Us</a></li>
+          <li><a href="#vehicles" class="hover:text-gray-500">Vehicles</a></li>
+          <li><a href="#dealers" class="hover:text-gray-500">Dealers</a></li>
+          <li><a href="#packages" class="hover:text-gray-500">Packages</a></li>
+          <li><a href="#blog" class="hover:text-gray-500">Blog</a></li>
+          <li><a href="#contact" class="hover:text-gray-500">Contact</a></li>
         </ul>
         
-        <div class="navbar-extra">
+        <!-- Pulsanti  -->
+        <div class="flex items-center space-x-3">
+          <button class="flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-black hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+            <img src="../assets/padlock.png" alt="My Account" class="h-5 w-5 mr-2" /> My account
+          </button>
           
-          <a href="#account" class="navbar-account">
-            <img src="../assets/padlock.png" alt="My Account"> My account
-          </a>
-          <a href="#addcar" class="navbar-account">
-            <img src="../assets/add.png" alt="Add Car"> Add Car 
-          </a>
-          <span class="navbar-phone">(+123) 456 7890</span>
+          <button class="flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-black hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+            <img src="../assets/add.png" alt="Add Car" class="h-5 w-5 mr-2" /> Add Car 
+          </button>
+
+          <button class="flex items-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-black hover:bg-gray-900 focus:outline-none focus:border-gray-900 focus:shadow-outline-gray transition ease-in-out duration-150">
+            <img src="../assets/headphones.png" alt="Phone" class="h-5 w-5 mr-2" /> +1 (234) 567 89 10
+          </button>
         </div>
       </nav>
     </header>
@@ -37,33 +40,6 @@
   </script>
   
   <style scoped>
-  .header {
-    background-color: white;
-    color: black;
-  }
   
-  .navbar {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  
-  .navbar-nav {
-    list-style: none;
-  }
-  
-  .navbar-nav li {
-    display: inline-block;
-    margin-right: 20px;
-  }
-  
-  .navbar-nav a {
-    color: black;
-    text-decoration: none;
-    
-  }
-  
-  .navbar-extra {
-  }
   </style>
   
