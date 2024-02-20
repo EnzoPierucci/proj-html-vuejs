9fcc93ee7ffa3191690500a0f4e410cf021bf86e
EnzoPierucci <enzopierucci@gmail.com>
2024-02-20 12:57:02 +0100

    file and comp

---
+++ proj-html-vuejs/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import HeaderComp from './components/Header.vue'
+import MainComp from './components/Main.vue'
+import FooterComp from './components/Footer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HeaderComp,
+    MainComp,
+    FooterComp
+  }
+};
+</script>
+
+<template>
+  
+</template>
+
+<style scoped>
+
+</style>
